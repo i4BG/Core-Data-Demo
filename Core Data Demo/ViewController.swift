b6b80020b0125c53d81c7d5c3e19852b22c6c25b
Ivaylo Todorov <ivaylo.k.todorov@me.com>
2015-12-07 21:59:59 +0200

    CoreData-iOS
    training Core Data on iOS

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        newUser.setValue("Rob", forKey: "username")
+        newUser.setValue("pass123", forKey: "password")
+        
+        do {
+            
+            try context.save()
+        } catch {
+            print("problem")
+        }
+        
+        let request = NSFetchRequest(entityName: "Users")
+        request.returnsObjectsAsFaults = false
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        do {
+            let results = try context.executeFetchRequest(request)
+        
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    print(result.valueForKey("username")!)
+                    print(result.valueForKey("password")!)
+                    
+                    
+                }
+            }
+        }
+            catch {
+                print("failed")
+            }
+        }
+        }
+
+
+
+    func didReceiveMemoryWarning() {
+     //   super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
-}
 
